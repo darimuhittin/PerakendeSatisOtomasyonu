65a6a8bc5fff81082db1e2fcee4d0764437094a0
darimuhittin <darimuhittin@gmail.com>
2019-03-26 19:33:51 +0300

    Kasa projesi başlangıç

--- Pera/Urun.cs
+++ Pera/Urun.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,9 @@ namespace Pera
     {
         [Key]
         public int ID { get; set; }
-        public string Ad { get; set; }
+        public int BarkodNo { get; set; }
         public int Fiyat { get; set; }
+        public string Ad { get; set; }
         public string Agirlik { get; set; }
         public DateTime? SKT { get; set; }
     }
