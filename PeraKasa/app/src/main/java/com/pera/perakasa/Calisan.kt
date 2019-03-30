1300f45fb6fa1d3281d5f2c6371b7d321a3f8766
darimuhittin <darimuhittin@gmail.com>
2019-03-31 00:46:37 +0300

    Calisan ve Giris sınıfları oluşturuldu. MainMenuActivity aktivitesi eklendi. MainActivity veritabanı bağlantısı kodlarından arındırıldı,ilgili kodlar Giris sınıfına taşındı.

---
+++ PeraKasa/app/src/main/java/com/pera/perakasa/Calisan.kt
@@ -0,0 +1,5 @@
+package com.pera.perakasa
+
+class Calisan(val id:Int,val tc:String,val sifre:String,val ad:String,val soyad:String,val telNo:String,val email:String,val pozisyon:String,val yetki:Boolean,val Maas:Float) {
+
+}
